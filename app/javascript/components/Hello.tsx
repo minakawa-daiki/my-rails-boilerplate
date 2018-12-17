430e018353bbb9ae625b3c03a16767781068b108
minakawa-daiki <minakawa.daiki.as@gmail.com>
2018-12-17 21:21:03 +0900

    [ADD] Prettier 追加

--- app/javascript/components/Hello.tsx
+++ app/javascript/components/Hello.tsx
@@ -1,38 +1,34 @@
-import * as React from 'react';
-import axios from 'axios'
+import * as React from "react";
+import axios from "axios";
 
 interface HelloProps {
-    hello: { title: string }
+  hello: { title: string };
 }
 
 interface HelloState {
-    name: string
+  name: string;
 }
 
 class Hello extends React.Component<HelloProps, HelloState> {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props)
-    }
+  componentDidMount() {
+    axios
+      .get("http://localhost:3000/api/home")
+      .then(results => {
+        const data = results.data;
+        console.log(data);
+      })
+      .catch(e => {
+        console.log("ERROR!! occurred in Backend.", e);
+      });
+  }
 
-    componentDidMount() {
-        axios
-            .get('http://localhost:3000/api/home')
-            .then(results => {
-              const data = results.data;
-              console.log(data);
-            }).catch((e) => {
-              console.log('ERROR!! occurred in Backend.', e)
-            });
-    }
-
-    render () {
-        return (
-          <>
-            Title: {this.props.hello.title}
-          </>
-        );
-    }
+  render() {
+    return <>Title: {this.props.hello.title}</>;
+  }
 }
 
-export default Hello
+export default Hello;
