5ed72ecfe73562d433f66a6e6c0af81d8e7dfaa5
minakawa-daiki <minakawa.daiki.as@gmail.com>
2018-12-17 22:38:41 +0900

    [ADD] RuboCop追加と実行

--- app/views/home/index.json.jbuilder
+++ app/views/home/index.json.jbuilder
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 json.hello do
   json.title 'Hello World!'
 end
--- config/initializers/react_rails.rb
+++ config/initializers/react_rails.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   config.react.camelize_props = true
 end
