5a71c44881bac47f3837321b4b29bd223b673cb6
minakawa-daiki <minakawa.daiki.as@gmail.com>
2018-12-17 20:51:47 +0900

    [UPDATE] TypeScriptに変更

---
+++ app/javascript/components/Hello.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import axios from 'axios'
+
+interface HelloProps {
+    hello: { title: string }
+}
+
+interface HelloState {
+    name: string
+}
+
+class Hello extends React.Component<HelloProps, HelloState> {
+
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+        axios
+            .get('http://localhost:3000/api/home')
+            .then(results => {
+              const data = results.data;
+              console.log(data);
+            }).catch((e) => {
+              console.log('ERROR!! occurred in Backend.', e)
+            });
+    }
+
+    render () {
+        return (
+          <>
+            Title: {this.props.hello.title}
+          </>
+        );
+    }
+}
+
+export default Hello
