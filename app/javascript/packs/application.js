c87bf402ac9d86481cfcd1f629981fafc5f5cb43
minakawa-daiki <minakawa.daiki.as@gmail.com>
2019-01-19 14:02:29 +0900

    [Update] フォルダ名をjsからtsに更新

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@
 // layout file, like app/views/layouts/application.html.erb
 
 // Support component names relative to this directory:
-var componentRequireContext = require.context("src/js", true)
+var componentRequireContext = require.context("src/ts", true)
 var ReactRailsUJS = require("react_ujs")
 ReactRailsUJS.useContext(componentRequireContext)
 import 'src/scss/application'
